afa9fa5aa7b49b72673d8fdcec909bf737de0784
skb127 <simoelkhejali@gmail.com>
2025-11-12 12:26:05 +0100

    Fix typo in Currency.None comment
    Corrected 'dont't' to 'don't' in the comment for the internal Currency.None field to improve clarity.

--- Bookify.Domain/Shared/Currency.cs
+++ Bookify.Domain/Shared/Currency.cs
@@ -1,7 +1,7 @@
 ﻿namespace Bookify.Domain.Shared;
 public record Currency
 {
-    internal static readonly Currency None = new(""); // We aren't going to return it from the list of all currencies, we dont't want to expose it, outside of the domain project (internal)
+    internal static readonly Currency None = new(""); // We aren't going to return it from the list of all currencies, we don't want to expose it, outside the domain project (internal)
     public static readonly Currency Eur = new("EUR");
     public static readonly Currency Usd = new("USD");
 
